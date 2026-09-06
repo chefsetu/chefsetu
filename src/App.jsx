9b58f432d37444428c75a4f3434d6efcb570d3a5
thesandeepkonda <kondasandeep56@gmail.com>
2026-09-06 12:10:37 +0530

    update cname

--- src/App.jsx
+++ src/App.jsx
@@ -11,21 +11,44 @@ import About from './components/About.jsx';
 import Contact from './components/Contact.jsx';
 import Footer from './components/Footer.jsx';
 
+const components = new Map([
+  ['navbar', Navbar],
+  ['hero', Hero],
+  ['jobListings', JobListings],
+  ['categories', Categories],
+  ['career', CareerSection],
+  ['whyChefSetu', WhyChefSetu],
+  ['howItWorks', HowItWorks],
+  ['cta', CTA],
+  ['employer', EmployerSection],
+  ['about', About],
+  ['contact', Contact],
+  ['footer', Footer],
+]);
+
 function App() {
+  const sections = [
+    'navbar',
+    'hero',
+    'jobListings',
+    'categories',
+    'career',
+    'whyChefSetu',
+    'howItWorks',
+    'cta',
+    'employer',
+    'about',
+    'contact',
+    'footer',
+  ];
+
   return (
     <div className="min-h-screen">
-      <Navbar />
-      <Hero />
-      <JobListings />
-      <Categories />
-      <CareerSection />
-      <WhyChefSetu />
-      <HowItWorks />
-      <CTA />
-      <EmployerSection />
-      <About />
-      <Contact />
-      <Footer />
+      {sections.map((section) => {
+        const Component = components.get(section);
+
+        return Component ? <Component key={section} /> : null;
+      })}
     </div>
   );
 }
